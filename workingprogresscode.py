e927701016edf8e268a77cd18a722cffab5c46ca
Jallal Abdelhadi <jallalabdelhadi@gmail.com>
2021-04-23 03:21:58 -0400

    Update workingprogresscode.py

--- workingprogresscode.py
+++ workingprogresscode.py
@@ -2,3 +2,9 @@ import time
 from datetime import datetime as dt
 # we use 'datetime' so we can set a combination of date AND time in the website restrictions
 # we use the import idiom "dt" to make the code more unambigious 
+
+hosts_path = "/etc/hosts"
+
+redict = "127.0.0.1"
+#we are working on the host file. we will be mapping hostnames of websites to our local address.
+
